0e39b57fdee64559a34848ab724ee0afd9a48432
Maximiliano Robles <sebans1988@gmail.com>
2022-12-04 17:10:53 -0300

    Entrega Proyecto Final v1.1

--- src/components/ItemList/ItemListContainer.jsx
+++ src/components/ItemList/ItemListContainer.jsx
@@ -10,14 +10,20 @@ export default function ItemListContainer() {
 
   useEffect(()=>{
     GetProducts(categoryId).then((res)=>{
-      setProducts(res)}
+      setProducts(res)
+    console.log(res)}
     )
   },[categoryId])
   if(products === null){
     return <section className="py-5" style={{minHeight: '100vh'}}>
         <div className='d-flex justify-content-center pt-5 mt-5'> <Loader/> </div>
       </section>
-  }else{
+  }else if (products.length === 0){
+    return <section className="py-5" style={{minHeight: '52vh'}}>
+        <h2 className='d-flex justify-content-center pt-5 mt-5'>Categoría Inexistente</h2>
+      </section>
+  }
+  else{
     return (
       <ItemList products={products}/>  
       )
